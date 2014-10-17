dcccf481c502eb4df20f7ea20a37215c10d01b10
root <root@celend.tk>
2014-10-18 00:22:25 +0800

    results page

--- index.php
+++ index.php
@@ -7,24 +7,28 @@
  * @date 14-10-15
  */
 define("QUOTE", TRUE);
-
-require_once 'google_search.class.php';
+error_reporting(E_ALL);
 require_once 'config.php';
-$c = file_get_contents('test3.html');
-preg_match('`href="/search\?q[^"]*?ei=([^"]*?)&[^"]*?"`s', $c, $e);
-var_dump($e);
-/*$q = $_GET['qqq'];
-$p = isset($_GET['ppp']) ? $_GET['ppp'] : 0;
-$d = isset($_GET['ddd']) ? $_GET['ddd'] : 'y';
-$n = isset($_GET['num'])? (int) $_GET['num'] : FALSE;
-$g = new Google_search($q);
-echo '<pre>';
-if($n)
-    $g->set_num($n);
-if($p)
-    $g->set_page($p);
-if($d)
-    $g->set_time_limit($d);
-$g->load();
-echo $g->get_full_url();
-var_dump($g->get_results());*/
+if(!isset($_GET['qqq'])){
+    require_once 'view.class.php';
+    $index = new view();
+    $index->show_index();
+}
+else{
+    require_once 'google_search.class.php';
+    $q = $_GET['qqq'];
+    $p = isset($_GET['ppp']) ? $_GET['ppp'] : 0;
+    $d = isset($_GET['ddd']) ? $_GET['ddd'] : 'y';
+    $n = isset($_GET['num'])? (int) $_GET['num'] : FALSE;
+    $g = new Google_search($q);
+    echo '<pre>';
+    if($n)
+        $g->set_num($n);
+    if($p)
+        $g->set_page($p);
+    if($d)
+        $g->set_time_limit($d);
+    $g->load();
+    var_dump($g->get_results());
+    echo $g->get_full_url();
+}
